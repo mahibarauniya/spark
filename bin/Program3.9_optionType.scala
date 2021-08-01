892e870d23bc784cdb730ca3940b626a3b634b77
Mahi Barauniya <mahimmm07@gmail.com>
2021-08-01 19:36:20 -0400

    option modificatino

--- bin/Program3.9_optionType.scala
+++ bin/Program3.9_optionType.scala
@@ -4,7 +4,7 @@
  
 
 object Program39_optionType extends App {
-  
+ /* 
  def getAString(num: Int) : Option[String] = {
     if (num >=0) Some("A Positive Number!!")
     else None
@@ -20,6 +20,20 @@ object Program39_optionType extends App {
  
 printResult(10)  //result: A Positive Number!!
 printResult(-10) //result: Not a valid number....
+*/
+  
 
-
+  case class Address ( city: String, state: String, zip: String)
+  
+  class User(email: String, password: String){
+    var firstname : Option[String] = None
+    var lastname : Option[String] = Some("Barauniya")
+    var address : Option[Address] = None
+  }
+  
+  var usr = new User ( "Mahimmmm07@gmail.com", "XYZ")
+  
+ println(usr.firstname.getOrElse("<Not assigned>")) // <Not assigned>
+ println(usr.lastname.getOrElse("<Not assigned>"))  //Barauniya
+     
 }
